3863c22a46dc3b12ad632061e1e587201a728988
zuhriddin <zuhriddinsattorov12@gmail.com>
2026-04-16 23:18:47 +0900

    feat: learn string in Python

--- primitive.py
+++ primitive.py
@@ -10,3 +10,19 @@ print(f"the count: {count} and type: {count_type}")
 result1 = count.bit_count() # method
 result2 = count.numerator # state
 print(result1, result2)
+
+print("===== string =======")
+#METHODS: upper() lower() title() find() replace()
+
+course = "AI Python FullStack"
+result = type(course)
+print(f"the result (1): {result}")
+
+result = course.title()
+print(f"the result (2): {result}")
+
+result = course.upper()
+print(f"the result (3): {result}")
+
+result = course.replace("FullStack", "MasterClass")
+print(f"the result (3): {result}")
