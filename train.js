852d1f0bbf93ceb9080aa2c00a2b79e8cee31555
zuhriddin <zuhriddinsattorov12@gmail.com>
2026-04-20 19:13:41 +0900

    feat: second mitTask

--- train.js
+++ train.js
@@ -7,8 +7,25 @@ Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkin
 MASALAN countLetter("e", "engineer") 3ni return qiladi.
 */
 
-function mitTask(mit, task) {
-  return task.split(mit).length - 1;
+// function mitTask(mit, task) {
+//   return task.split(mit).length - 1;
+// }
+
+// console.log(mitTask("e", "engineer"));
+
+/* MitTask #2 
+
+B-TASK (Nodejs) Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
+*/
+
+function mitTask(str) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] >= "0" && str[i] <= "9") {
+      count++;
+    }
+  }
+  return count;
 }
 
-console.log(mitTask("e", "engineer"));
+console.log(mitTask("Year 2005")); // 4
