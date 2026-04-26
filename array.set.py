41ed9aef1f9ae8e98e7034c8c3e924fe247f9cb5
zuhriddin <zuhriddinsattorov12@gmail.com>
2026-04-27 06:09:07 +0900

    feat: specific operators with set

--- array.set.py
+++ array.set.py
@@ -21,4 +21,33 @@ print("numbers(3):", numbers)
 del numbers[0:2]
 print("numbers(4)", numbers)
 
-print("===== array=====")
+print("===== Set =====")
+# { Set } of unique collection without keeping order!
+new_numbers = array("i", [1, 4, 7, 5, 7, 5, 4, 7, 8, 4, 41])
+numbs_set = set(new_numbers)
+
+print(f"the numbs_set: {numbs_set} and type: {type(numbs_set)}")
+
+numbs_set.add(200)
+print("numbs_set(1):", numbs_set)
+
+numbs_set.add(7)
+print("numbs_set(2):", numbs_set)
+
+
+print("======= Specific operators: | & - ^  =====")
+
+
+a = {10, 20, 50}
+b = {20, 40}
+
+result1 = a | b # union
+result2 = a & b # intersection
+result3 = a - b # difference
+result4 = a ^ b # symmetric differene
+
+
+print("result1:", result1)
+print("result2:", result2)
+print("result3:", result3)
+print("result4:", result4)
