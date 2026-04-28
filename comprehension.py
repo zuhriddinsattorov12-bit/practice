c9242fd3305f437d4ad731724aa3aa741b01ed98
zuhriddin <zuhriddinsattorov12@gmail.com>
2026-04-28 21:13:27 +0900

    feat: study comprehension & list comprehension

---
+++ comprehension.py
@@ -0,0 +1,36 @@
+''' Comprehention
+    (1) What is comprehension & list comp.
+    (2) set and dictionary comp.
+'''
+
+print("===== What is comprehension & list comp. =====")
+# Comprehension acts like spread operator!
+
+''' Comprehension general syntax:
+    a) *iterable  > 
+    b) <expression> for item in iterable               > 
+    c) <expression> for item in iterable <condition>   > filter 
+'''
+
+# list comp.
+numbers = [1, 2, 4, 2, 1, 20]  # a version
+list_numbers = [*numbers]      # [*numbers]  > comprehension
+print("list_numbers:", list_numbers)
+print(numbers is list_numbers)  # is > ....mi ?
+print(id(numbers), id(list_numbers)) # id
+
+print("========")
+people = [("Robert", 20), ("Steve", 19), ("Joseph", 25)]
+list_people = [person[0]for person in people] # b version
+print("list_people:", list_people)
+
+print("====== C version >= Filter =====")
+cars = [
+    ("Ferrari", 78),
+    ("Toyota", 87),
+    ("Audi", 116),
+    ("BMW", 109),
+    ("Pagani", 33)
+]
+list_cars = [car[0] for car in cars if car[1] > 80]      # c version
+print("list_cars:", list_cars)
